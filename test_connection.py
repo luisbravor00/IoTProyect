0c5e3a963928d91bd5a3f8308cec5671ca1e58b8
Oneballnote <oneballnotita@gmail.com>
2024-03-07 09:06:58 -0600

    Hotfix en el nombre de cs, no jala si se llama DSN la variable de entorno

--- test_connection.py
+++ test_connection.py
@@ -6,15 +6,15 @@ load_dotenv()
 dbuser = os.getenv('DATABASE_USER')
 dbpswd = os.getenv('DATABASE_PSWD')
 dbdir = os.getenv('DIR_LOCATION')
-cs = os.getenv('DSN')
-w_pswd = os.getenv('WALLET_PSWD')
+cs = os.getenv('CONN_STR')
 
 
+
+print(dbdir)
     
-connection = oracledb.connect(config_dir = dbdir,
-                              user= dbuser, password=dbpswd, dsn=cs,
-                              wallet_location = dbdir,
-                              wallet_password=w_pswd)
+connection = oracledb.connect(config_dir = dbdir,  user= dbuser, 
+                              password=dbpswd, dsn=cs, 
+                              wallet_location = dbdir, wallet_password=dbpswd)
 
 cursor = connection.cursor()
 
