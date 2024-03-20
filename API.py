12d11f1caa5711a19e9e91e36e87fdcda182b123
KaysenGamiz <k.aiseen@hotmail.com>
2024-03-20 12:30:04 -0600

    [Routes] Medicine Routes Added

--- API.py
+++ API.py
@@ -2,12 +2,14 @@ from flask import Flask
 from routes.prescriptions_route import prescriptions_blueprint
 from routes.patients_route import patients_blueprint
 from routes.doctors_route import doctors_blueprint
+from routes.medicine_route import medicine_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(prescriptions_blueprint, url_prefix='/prescriptions')
 app.register_blueprint(patients_blueprint, url_prefix='/patients')
 app.register_blueprint(doctors_blueprint, url_prefix='/doctors')
+app.register_blueprint(medicine_blueprint, url_prefix='/medicine')
 
 if __name__ == '__main__':
     app.run(port=7070)
