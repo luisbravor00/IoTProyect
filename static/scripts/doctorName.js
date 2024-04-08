719fb5917e0ff14cc4823ba587e9cec032d4b0da
Oneballnote <oneballnotita@gmail.com>
2024-04-08 11:11:19 -0600

    pagina doctor y main casi terminadas

---
+++ static/scripts/doctorName.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $.get("url", function(data){
+        let doctorArr = $('.doctor-name');
+
+        doctorArr.forEach(element => {
+             
+        });
+    })
+});
---
+++ static/scripts/navBar&footer.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    let navbar = `
+                <a href="./patients.html">Inicio</a>
+                <a href="./patients.html">Patients</a>
+                <a href="./doctors.html">Doctors</a>
+                <a href="./login.html">Log in</a>
+                `;
+    $('.navbar').append(navbar);
+
+    let footer = `
+                <p>CareMinder S.A de C.V</p>
+                <div class="icons">
+                  <a target="_blank" href="https://web.telegram.org/a/"><i class="footer-icon fa-brands fa-telegram"></i></a>
+                  <a target="_blank" href="https://www.facebook.com/"><i class="footer-icon fa-brands fa-facebook"></i></a>
+                  <a target="_blank" href="https://twitter.com/?lang=es"><i class="footer-icon fa-brands fa-x-twitter"></i></a>
+                  <a target="_blank" href="https://www.twitch.tv/illojuan"><i class="footer-icon fa-brands fa-twitch"></i></a>
+                  <a target="_blank" href="https://www.tumblr.com/"><i class="footer-icon fa-brands fa-tumblr"></i></a>
+                  <a target="_blank" href="https://www.youtube.com/@jluiselvira"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+                `;
+    $('footer').append(footer);
+});
